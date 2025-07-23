260fb89429749c77f10add3a78f45bd83dc901cc
Alemarval33 <alemarval33@gmail.com>
2025-07-23 19:58:25 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
