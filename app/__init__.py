d64d843a5742dc70e51e9b0f1682cc051d9f9b58
Alemarval33 <alemarval33@gmail.com>
2025-07-23 19:29:29 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,3 +8,19 @@ def create_app():
         return "¡Bienvenido a ZunekoApp!"
 
     return app
+
+from flask import render_template, redirect, url_for, flash, request
+from app.forms import RegistrationForm
+from app.models import User
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        if User.get_by_email(form.email.data):
+            flash('Ese email ya está registrado.', 'danger')
+        else:
+            user = User.create(form.email.data, form.password.data)
+            flash('Usuario creado correctamente. Ahora podés iniciar sesión.', 'success')
+            return redirect(url_for('login'))
+    return render_template('register.html', form=form)
