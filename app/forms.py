9bf5b4f0564a1d9d3ab4747ef9c62c9f176d97ae
Alemarval33 <alemarval33@gmail.com>
2025-07-23 19:56:42 -0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,9 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Correo electrónico', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=120)])
     password = PasswordField('Contraseña', validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField('Confirmar contraseña', validators=[
-        DataRequired(), EqualTo('password', message='Las contraseñas deben coincidir')
-    ])
+    confirm_password = PasswordField('Confirmar Contraseña', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Registrarse')
