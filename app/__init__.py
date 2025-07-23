2d820901c9b22330152418bde9e2dcf6c76e3a9e
Alemarval33 <alemarval33@gmail.com>
2025-07-23 19:49:37 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, flash, request
+from app.forms import RegistrationForm
+from app.models import User
 
 def create_app():
     app = Flask(__name__)
@@ -7,20 +9,16 @@ def create_app():
     def index():
         return "¡Bienvenido a ZunekoApp!"
 
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            if User.get_by_email(form.email.data):
+                flash('Ese email ya está registrado.', 'danger')
+            else:
+                user = User.create(form.email.data, form.password.data)
+                flash('Usuario creado correctamente. Ahora podés iniciar sesión.', 'success')
+                return redirect(url_for('login'))
+        return render_template('register.html', form=form)
+
     return app
-
-from flask import render_template, redirect, url_for, flash, request
-from app.forms import RegistrationForm
-from app.models import User
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        if User.get_by_email(form.email.data):
-            flash('Ese email ya está registrado.', 'danger')
-        else:
-            user = User.create(form.email.data, form.password.data)
-            flash('Usuario creado correctamente. Ahora podés iniciar sesión.', 'success')
-            return redirect(url_for('login'))
-    return render_template('register.html', form=form)
