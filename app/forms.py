35747414c54e86ad3a079d68c807c0ae0c0e737d
Alemarval33 <alemarval33@gmail.com>
2025-07-23 22:03:51 -0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,12 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Contraseña', validators=[DataRequired(), Length(min=6)])
     activo = BooleanField('Usuario activo', default=True)
     submit = SubmitField('Registrarse')
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    remember = BooleanField('Recordarme')
+    submit = SubmitField('Ingresar')
